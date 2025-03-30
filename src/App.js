b379e613e3dd9d0f4b11a5be470ae5aa8696f98e
fatmaAlzu <fatma.alzubaidi1@ucalgary.ca>
2025-03-29 22:21:34 -0600

    better

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import LoginForm from "./components/LoginForm";
-import Courses from "./components/CourseCatalog"; 
+import EnrollmentList from "./components/EnrollmentList";
 import EnrolledCourse from "./components/EnrolledCourse";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
@@ -13,7 +13,7 @@ const App = () => {
         <Header />
         <Routes>
           <Route path="/" element={<LoginForm />} />
-          <Route path="/courses" element={<Courses />} />
+          <Route path="/courses" element={<EnrollmentList />} /> {/* This is now the courses page */}
           <Route path="/enrolled" element={<EnrolledCourse />} />
         </Routes>
         <Footer />
